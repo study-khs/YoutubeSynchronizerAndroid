538f1722047456120ae634d1fd7ede67088923c8
pjo901018 <pjo901018@korea.ac.kr>
2017-04-15 16:04:35 +0900

    Server에 get보내기 구현

--- app/src/main/java/khs/study/youtubesynchronizerandroid/retrofit/UserJoinWithFacebookNetwork.java
+++ app/src/main/java/khs/study/youtubesynchronizerandroid/retrofit/UserJoinWithFacebookNetwork.java
@@ -1,19 +1,15 @@
 package khs.study.youtubesynchronizerandroid.retrofit;
 
-import khs.study.youtubesynchronizerandroid.domain.User;
-import khs.study.youtubesynchronizerandroid.domain.UserJoinWithFacebookRequestDto;
+import khs.study.youtubesynchronizerandroid.domain.ApiResponseDto;
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
 
 /**
  * Created by jaeyoung on 2017. 4. 15..
  */
 
 public interface UserJoinWithFacebookNetwork {
-    @GET("api/user_join_with_facebook_network")
-    Call<User> getUserJoinWithFacebookNetwork(@Header("type") String type, @Header("token") String token);
+    @GET("loginWithFacebook")
+    Call<ApiResponseDto> getUserJoinWithFacebookNetwork(@Header("userType") String type, @Header("token") String token);
 }
