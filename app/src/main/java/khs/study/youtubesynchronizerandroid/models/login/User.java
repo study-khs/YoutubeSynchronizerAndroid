0bfc10cfa0592a03506a00d7da401dc0c91f9949
pjo901018 <pjo901018@korea.ac.kr>
2017-06-03 13:33:37 +0900

    Login위한 데이터 모델 정의

---
+++ app/src/main/java/khs/study/youtubesynchronizerandroid/models/login/User.java
@@ -0,0 +1,12 @@
+package khs.study.youtubesynchronizerandroid.models.login;
+
+/**
+ * Created by jaeyoung on 2017. 6. 3..
+ */
+
+public class User {
+    private String userLoginId;
+    private String userNickname;
+    private String userPassword;
+    private String userType;
+}
---
+++ app/src/main/java/khs/study/youtubesynchronizerandroid/models/login/UserLoginForm.java
@@ -0,0 +1,11 @@
+package khs.study.youtubesynchronizerandroid.models.login;
+
+/**
+ * Created by jaeyoung on 2017. 6. 3..
+ */
+
+public class UserLoginForm {
+    private String userType;
+    private String userLoginId;
+    private String userPassword;
+}
